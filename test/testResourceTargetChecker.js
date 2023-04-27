623354324276824e1046f95f1dd3be9c398d229e
plizon <plizon@box.com>
2023-04-27 15:21:54 +0200

    fix tests

--- test/testResourceTargetChecker.js
+++ test/testResourceTargetChecker.js
@@ -675,21 +675,6 @@ export const testResourceTargetChecker = {
         );
         test.ok(rule);
 
-        // const actual = rule.match({
-        //     locale: "ja-JP",
-        //     resource: new ResourceString({
-        //         key: "matcher.test",
-        //         sourceLocale: "en-US",
-        //         source: "Box Embed Widget",
-        //         targetLocale: "ja-JP",
-        //         target: "Box 埋め込みウィジェット",
-        //         pathName: "a/b/c.xliff"
-        //     }),
-        //     file: "x/y"
-        // });
-        // console.log(actual);
-        // test.ok(actual);
-
         const subject = {
             locale: "ja-JP",
             resource: new ResourceString({
@@ -704,7 +689,6 @@ export const testResourceTargetChecker = {
         };
 
         const result = rule.match(subject);
-        console.log(result)
         test.deepEqual(result, [new Result({
             rule,
             severity: "warning",
@@ -712,7 +696,7 @@ export const testResourceTargetChecker = {
             pathName: "x/y",
             source: "Box Embed Widget",
             id: "matcher.test",
-            description: "Ensure that the target does not contain a space character between a double-byte kana and single-byte character.",
+            description: 'The space character is not allowed in the target string. Remove the space character.',
             highlight: 'Target: Bo<e0>x 埋</e0>め込みウィジェット',
         })]);
 
