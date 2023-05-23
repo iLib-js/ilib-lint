27d02631f738d9c4016b3587f80277529f43ecb9
Wadim Wawrzenczak <wwawrzenczak@box.com>
2023-05-23 16:51:34 +0200

    Remove leftover test code

--- test/testStringFixer.js
+++ test/testStringFixer.js
@@ -1,16 +1,6 @@
 import { IntermediateRepresentation } from "i18nlint-common";
 import StringFixer, { StringFix } from "../src/plugins/StringFixer.js";
 
-let processed = StringFixer.applyCommands("12345678", [
-    StringFix.commands.insertAfter(0, "0"), // add 0 before 1
-    StringFix.commands.deleteAfter(1, 1), // delete "2",
-    StringFix.commands.insertAfter(8, "9"), // add 9 after 8
-    StringFix.commands.deleteAfter(5, 2), // delete "67"
-]);
-
-console.log(processed);
-console.log(processed === "0134589");
-
 export const testStringFixer = {
     stringFixerShouldInsert: function (test) {
         test.expect(2);
