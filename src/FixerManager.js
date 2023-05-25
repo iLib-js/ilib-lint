08f7c78b7930b1c47e16e8f13ee96715cd649b68
Wadim Wawrzenczak <wwawrzenczak@box.com>
2023-05-25 14:54:05 +0200

    Fix file header date

--- src/FixerManager.js
+++ src/FixerManager.js
@@ -1,7 +1,7 @@
 /*
  * FixerManager.js
  *
- * Copyright © 2022-2023 JEDLSoft
+ * Copyright © 2023 JEDLSoft
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
