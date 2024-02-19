fa7095138423c7fc7c2a66ca712a2fe31f4bee76
Edwin Hoogerbeets <ehoogerbeets@box.com>
2024-02-19 11:07:52 -0800

    Add in the old plugin to test whether plugin loading
    - the code should refuse to load old plugins, as they are
    incompatible

---
+++ test/ilib-lint-plugin-test-old/src/index.js
@@ -0,0 +1,70 @@
+/*
+ * index.js - main program of ilib-lint plugin test
+ *
+ * Copyright © 2022 JEDLSoft
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { Plugin } from 'i18nlint-common';
+
+import TestParser from './TestParser.js';
+import TestRule from './TestRule.js';
+import TestFormatter from './TestFormatter.js';
+import TestFixer from './TestFixer.js';
+
+class TestPlugin extends Plugin {
+    constructor(options) {
+        super(options);
+    }
+
+    init() {
+        //console.log("TestPlugin.init() called");
+    }
+
+    getExtensions() {
+        //console.log("TestPlugin.getExtensions() called");
+        return [ "xyz" ];
+    }
+
+    getRules() {
+        //console.log("TestPlugin.getRules() called");
+        return [ TestRule ];
+    }
+
+    getRuleSets() {
+        return {
+            "test": {
+                "resource-test": true
+            }
+        };
+    }
+
+    getParsers() {
+        //console.log("TestPlugin.getParsers() called");
+        return [ TestParser ];
+    }
+
+    getFormatters() {
+        //console.log("TestPlugin.getFormatters() called");
+        return [ TestFormatter ];
+    }
+
+    getFixers() {
+        return [ TestFixer ];
+    }
+
+}
+
+export default TestPlugin;
